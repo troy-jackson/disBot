835923ae38ec7df30da44661c2d30c3695a4e715
Troy Jackson <taj6eb@virginia.edu>
2021-09-17 20:17:16 -0400

    added event handling

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 
 client.once('ready', () => {
 	console.log('Chiaki is online!');
